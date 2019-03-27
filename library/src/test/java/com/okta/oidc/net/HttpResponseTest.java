3999fc32d5d75a77a75c21d260f57d7a972bbb03
Fei Chen <fei.chen@okta.com>
2019-03-27 01:09:56 -0700

    Fix http response tests

--- library/src/test/java/com/okta/oidc/net/HttpResponseTest.java
+++ library/src/test/java/com/okta/oidc/net/HttpResponseTest.java
@@ -16,9 +16,11 @@ package com.okta.oidc.net;
 
 import android.net.Uri;
 
+import com.google.gson.Gson;
 import com.okta.oidc.AuthenticateClient;
 import com.okta.oidc.OIDCAccount;
 import com.okta.oidc.net.request.AuthorizedRequest;
+import com.okta.oidc.net.response.TokenResponse;
 import com.okta.oidc.util.JsonStrings;
 import com.okta.oidc.util.MockEndPoint;
 import com.okta.oidc.util.TestValues;
@@ -29,12 +31,15 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Collections;
@@ -43,130 +48,134 @@ import java.util.List;
 import java.util.Map;
 
 import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
 
 import static com.okta.oidc.net.HttpConnection.CONTENT_TYPE;
 import static com.okta.oidc.net.HttpConnection.JSON_CONTENT_TYPE;
-import static com.okta.oidc.util.TestValues.CUSTOM_STATE;
-import static com.okta.oidc.util.TestValues.CUSTOM_URL;
+import static com.okta.oidc.util.JsonStrings.CONFIGURATION_NOT_FOUND;
+import static com.okta.oidc.util.JsonStrings.FORBIDDEN;
+import static com.okta.oidc.util.JsonStrings.TOKEN_SUCCESS;
+import static com.okta.oidc.util.JsonStrings.WWW_AUTHENTICATE;
+import static java.net.HttpURLConnection.HTTP_FORBIDDEN;
 import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static java.net.HttpURLConnection.HTTP_OK;
-import static java.net.HttpURLConnection.HTTP_UNAUTHORIZED;
 import static org.junit.Assert.*;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = 27)
 public class HttpResponseTest {
-    private HttpResponse mOkEmptyResponse;
-    private HttpResponse mOkJsonResponse;
-    private HttpResponse mInvalidJsonResponse;
-    private HttpResponse mInvalidEmptyResponse;
+    @Rule
+    public final MockWebServer mServer = new MockWebServer();
 
-    private MockEndPoint mEndPoint;
+    @Rule
+    public Timeout mGlobalTimeout = Timeout.seconds(30);
+
     @Rule
     public ExpectedException mExpectedEx = ExpectedException.none();
 
-    @Before
-    public void setUp() throws Exception {
-        mEndPoint = new MockEndPoint();
-        setupOkEmptyResponse();
-        setupOkJsonResponse();
-        setupInvalidEmptyResponse();
-        setupInvalidJsonResponse();
+    @Test
+    public void textOKResponse() throws Exception {
+        mServer.enqueue(new MockResponse().setBody("test"));
+
+        URL url = mServer.url("/").url();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Accept-Language", "en-US");
+        connection.setRequestProperty("Content-Length", "4");
+        HttpResponse response = new HttpResponse(HTTP_OK, connection.getHeaderFields(),
+                connection.getContentLength(), connection);
+
+        Map<String, List<String>> headers = response.getHeaders();
+        InputStream in = response.getContent();
+        int contentLength = response.getContentLength();
+        int status = response.getStatusCode();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        assertEquals(reader.readLine(), "test");
+        assertEquals(HTTP_OK, status);
+        assertNotNull(headers);
+        assertEquals(contentLength, 4);
+
+        RecordedRequest request = mServer.takeRequest();
+        assertEquals(request.getRequestLine(), "GET / HTTP/1.1");
+        assertEquals(request.getHeader("Accept-Language"), "en-US");
     }
 
     @Test
-    public void getStatusCode() {
-        int status = mOkEmptyResponse.getStatusCode();
+    public void textErrorResponse() throws Exception {
+        mServer.enqueue(new MockResponse().setResponseCode(HTTP_FORBIDDEN)
+                .addHeader(CONTENT_TYPE, "text/plain")
+                .addHeader(WWW_AUTHENTICATE, FORBIDDEN)
+                .setBody("Forbidden"));
+
+        URL url = mServer.url("/").url();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Accept-Language", "en-US");
+        connection.setRequestProperty("Content-Length", "9");
+        HttpResponse response = new HttpResponse(HTTP_FORBIDDEN, connection.getHeaderFields(),
+                connection.getContentLength(), connection);
+
+        Map<String, List<String>> headers = response.getHeaders();
+        InputStream in = response.getContent();
+        int contentLength = response.getContentLength();
+        int status = response.getStatusCode();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        assertEquals(reader.readLine(), "Forbidden");
+        assertEquals(HTTP_FORBIDDEN, status);
+        assertNotNull(headers);
+        assertTrue(headers.get(WWW_AUTHENTICATE).contains(FORBIDDEN));
+        assertEquals(contentLength, 9);
+    }
+
+    @Test
+    public void jsonOKResponse() throws Exception {
+        mServer.enqueue(new MockResponse().setResponseCode(HTTP_OK)
+                .addHeader(CONTENT_TYPE, JSON_CONTENT_TYPE)
+                .setBody(TOKEN_SUCCESS));
+
+        URL url = mServer.url("/").url();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Accept-Language", "en-US");
+        connection.setRequestProperty("Content-Length", TOKEN_SUCCESS.length() + "");
+        HttpResponse response = new HttpResponse(HTTP_OK, connection.getHeaderFields(),
+                connection.getContentLength(), connection);
+
+        Map<String, List<String>> headers = response.getHeaders();
+        InputStream in = response.getContent();
+        int contentLength = response.getContentLength();
+        int status = response.getStatusCode();
+        JSONObject jsonObject = response.asJson();
+        TokenResponse tokenResponse =
+                new Gson().fromJson(jsonObject.toString(), TokenResponse.class);
+
+        assertNotNull(tokenResponse);
         assertEquals(HTTP_OK, status);
+        assertNotNull(headers);
+        assertEquals(contentLength, TOKEN_SUCCESS.length());
 
-        status = mOkJsonResponse.getStatusCode();
-        assertEquals(HTTP_OK, status);
+    }
 
-        status = mInvalidEmptyResponse.getStatusCode();
-        assertEquals(HTTP_UNAUTHORIZED, status);
+    @Test
+    public void jsonErrorResponse() throws Exception {
+        mServer.enqueue(new MockResponse().setResponseCode(HTTP_NOT_FOUND)
+                .addHeader(CONTENT_TYPE, JSON_CONTENT_TYPE)
+                .setBody(CONFIGURATION_NOT_FOUND));
 
-        status = mInvalidJsonResponse.getStatusCode();
+        URL url = mServer.url("/").url();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Accept-Language", "en-US");
+        connection.setRequestProperty("Content-Length", CONFIGURATION_NOT_FOUND.length() + "");
+        HttpResponse response = new HttpResponse(HTTP_NOT_FOUND, connection.getHeaderFields(),
+                connection.getContentLength(), connection);
+
+        Map<String, List<String>> headers = response.getHeaders();
+        InputStream in = response.getContent();
+        int contentLength = response.getContentLength();
+        int status = response.getStatusCode();
+
         assertEquals(HTTP_NOT_FOUND, status);
-    }
-
-    @Test
-    public void getHeaders() throws InterruptedException {
-        Map<String, List<String>> headers = mOkEmptyResponse.getHeaders();
         assertNotNull(headers);
-        headers = mOkJsonResponse.getHeaders();
-        assertNotNull(headers);
-        headers = mInvalidEmptyResponse.getHeaders();
-        assertNotNull(headers);
-        headers = mInvalidJsonResponse.getHeaders();
-        assertNotNull(headers);
+        assertEquals(contentLength, CONFIGURATION_NOT_FOUND.length());
     }
-
-    @Test
-    public void getContentLength() {
-        int contentLength = mOkEmptyResponse.getContentLength();
-        System.out.println("content length = " + contentLength);
-        contentLength = mOkJsonResponse.getContentLength();
-        System.out.println("content length = " + contentLength);
-        contentLength = mInvalidEmptyResponse.getContentLength();
-        System.out.println("content length = " + contentLength);
-        contentLength = mInvalidJsonResponse.getContentLength();
-        System.out.println("content length = " + contentLength);
-    }
-
-    @Test
-    public void getContent() {
-        InputStream content = mOkJsonResponse.getContent();
-        assertNotNull(content);
-    }
-
-    @Test
-    public void disconnect() {
-    }
-
-    @Test
-    public void asJson() {
-    }
-
-    private void setupOkEmptyResponse() throws IOException, InterruptedException {
-        MockResponse mockResponse = mEndPoint.enqueueReturnSuccessEmptyBody();
-        URL url = mEndPoint.getHttpUrl().url();
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty("Accept-Language", "en-US");
-        mOkEmptyResponse = new HttpResponse(HTTP_OK, mockResponse.getHeaders().toMultimap());
-        //mEndPoint.takeRequest();
-    }
-
-    private void setupOkJsonResponse() throws IOException, InterruptedException {
-        MockResponse mockResponse = mEndPoint.enqueueUserInfoSuccess();
-        URL url = mEndPoint.getHttpUrl().url();
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty("Accept-Language", "en-US");
-        connection.setRequestProperty("Accept", HttpConnection.JSON_CONTENT_TYPE);
-
-        mOkJsonResponse = new HttpResponse(HTTP_OK, mockResponse.getHeaders().toMultimap(),
-                connection.getContentLength(), connection);
-        //mEndPoint.takeRequest();
-    }
-
-    private void setupInvalidEmptyResponse() throws IOException, InterruptedException {
-        MockResponse mockResponse = mEndPoint.enqueueReturnUnauthorizedRevoked();
-        URL url = mEndPoint.getHttpUrl().url();
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty("Accept-Language", "en-US");
-        mInvalidEmptyResponse = new HttpResponse(HTTP_UNAUTHORIZED, mockResponse.getHeaders().toMultimap());
-        mEndPoint.takeRequest();
-    }
-
-    private void setupInvalidJsonResponse() throws IOException, InterruptedException {
-        MockResponse mockResponse = mEndPoint.enqueueConfigurationFailure();
-        URL url = mEndPoint.getHttpUrl().url();
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty("Accept-Language", "en-US");
-        connection.setRequestProperty("Accept", HttpConnection.JSON_CONTENT_TYPE);
-        mInvalidJsonResponse = new HttpResponse(HTTP_NOT_FOUND, mockResponse.getHeaders().toMultimap(),
-                connection.getContentLength(), connection);
-        mEndPoint.takeRequest();
-    }
-
-
 }
