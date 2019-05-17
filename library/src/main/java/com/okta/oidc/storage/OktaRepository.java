d196569b9f12b524b048e9c72ac69a0ee4eb65c5
Fei Chen <fei.chen@okta.com>
2019-05-16 17:37:05 -0700

    Fix cache storage not saving data.

--- library/src/main/java/com/okta/oidc/storage/OktaRepository.java
+++ library/src/main/java/com/okta/oidc/storage/OktaRepository.java
@@ -62,7 +62,7 @@ public class OktaRepository {
                         getEncrypted(persistable.persist()));
             }
             cacheStorage.put(getHashed(persistable.getKey()),
-                    getEncrypted(persistable.persist()));
+                    persistable.persist());
         }
     }
 
