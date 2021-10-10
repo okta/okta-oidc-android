e1c5c2a57549345133621dd419660e1d72cf8302
Erik Manor <erik.manor@okta.com>
2021-10-09 22:56:24 -0700

    fix style check errors

--- app/src/main/java/com/okta/oidc/example/SampleActivity.java
+++ app/src/main/java/com/okta/oidc/example/SampleActivity.java
@@ -16,6 +16,7 @@
 package com.okta.oidc.example;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -57,6 +58,8 @@ import com.okta.oidc.results.Result;
 import com.okta.oidc.storage.SharedPreferenceStorage;
 import com.okta.oidc.storage.security.DefaultEncryptionManager;
 import com.okta.oidc.util.AuthorizationException;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -454,16 +457,13 @@ public class SampleActivity extends AppCompatActivity implements SignInDialog.Si
         });
 
         mSignInDeviceSecret.setOnClickListener(v -> {
-            String actor = getSharedPreferences(SampleActivity.class.getName(), MODE_PRIVATE)
-                    .getString(PREF_DEV_SECRET_ACTOR, null);
-            String subject = getSharedPreferences(SampleActivity.class.getName(), MODE_PRIVATE)
-                    .getString(PREF_DEV_SECRET_SUBJECT, null);
+            NativeSSOTokens tokens = getNativeSSOTokens();
 
-            if (actor == null || subject == null) {
+            if (tokens.secret == null || tokens.subject == null) {
                 Log.e(TAG, "actor or subject tokens could not be retrieved from storage");
                 return;
             }
-            mAuthClient.signIn(actor, subject,
+            mAuthClient.signIn(tokens.secret, tokens.subject,
                     new RequestCallback<Result, AuthorizationException>() {
                         @Override
                         public void onSuccess(@NonNull Result result) {
@@ -478,11 +478,13 @@ public class SampleActivity extends AppCompatActivity implements SignInDialog.Si
 
                             try {
                                 Log.d(TAG, "new id token: " +
-                                    mSessionClient.getTokens().getIdToken());
+                                        mSessionClient.getTokens().getIdToken());
                             } catch (Exception e) {
-                                Log.e(TAG, e.getMessage()); e.printStackTrace();
+                                Log.e(TAG, e.getMessage());
+                                e.printStackTrace();
                             }
                         }
+
                         @Override
                         public void onError(String error, AuthorizationException exception) {
                             Log.d(TAG, exception.error +
@@ -607,19 +609,14 @@ public class SampleActivity extends AppCompatActivity implements SignInDialog.Si
                     public void onSuccess(@NonNull AuthorizationStatus status) {
                         if (status == AuthorizationStatus.AUTHORIZED) {
                             mTvStatus.setText("authentication authorized");
-
                             SessionClient client = getSessionClient();
+
                             try {
-                                Log.d(TAG, "secret: " + client.getTokens().getDeviceSecret());
-                                Log.d(TAG, "subject: " + client.getTokens().getIdToken());
-                                getSharedPreferences(SampleActivity.class.getName(),
-                                    MODE_PRIVATE).edit()
-                                    .putString(PREF_DEV_SECRET_ACTOR,
-                                    client.getTokens().getDeviceSecret()).apply();
-                                getSharedPreferences(SampleActivity.class.getName(),
-                                    MODE_PRIVATE).edit()
-                                    .putString(PREF_DEV_SECRET_SUBJECT,
-                                    client.getTokens().getIdToken()).apply();
+                                String secret = client.getTokens().getDeviceSecret();
+                                String subject = client.getTokens().getIdToken();
+                                Log.d(TAG, "secret: " + secret);
+                                Log.d(TAG, "subject: " + subject);
+                                saveNativeSSOTokens(new NativeSSOTokens(secret, subject));
                             } catch (AuthorizationException ae) {
                                 Log.e(TAG, "Error saving client secret: " +
                                         ae.getMessage());
@@ -790,43 +787,40 @@ public class SampleActivity extends AppCompatActivity implements SignInDialog.Si
                             public void handleSuccess(AuthenticationResponse successResponse) {
                                 String sessionToken = successResponse.getSessionToken();
                                 mAuthClient.signIn(sessionToken, mPayload,
-                                new RequestCallback<Result,
-                                        AuthorizationException>() {
-                                    @Override
-                                    public void onSuccess(
-                                            @NonNull Result result) {
-                                        mTvStatus.setText("authentication authorized");
-                                        mIsSessionSignIn = true;
-                                        showAuthenticatedMode();
-                                        showNetworkProgress(false);
+                                    new RequestCallback<Result,
+                                            AuthorizationException>() {
+                                        @Override
+                                        public void onSuccess(
+                                                @NonNull Result result) {
+                                            mTvStatus.setText("authentication authorized");
+                                            mIsSessionSignIn = true;
+                                            showAuthenticatedMode();
+                                            showNetworkProgress(false);
+                                            SessionClient client = getSessionClient();
 
-                                        SessionClient client = getSessionClient();
-                                        try {
-                                            Log.d(TAG, "secret: " +
-                                                client.getTokens().getDeviceSecret());
-                                            Log.d(TAG, "subject: " +
-                                                client.getTokens().getIdToken());
-                                            getSharedPreferences(SampleActivity.class.getName(),
-                                                MODE_PRIVATE).edit()
-                                                .putString(PREF_DEV_SECRET_ACTOR, client
-                                                        .getTokens().getDeviceSecret()).apply();
-                                            getSharedPreferences(SampleActivity.class.getName(),
-                                                MODE_PRIVATE).edit()
-                                                    .putString(PREF_DEV_SECRET_SUBJECT,
-                                                        client.getTokens().getIdToken()).apply();
-                                        } catch (AuthorizationException ae) {
-                                            Log.e(TAG, "Error saving client secret: " +
-                                                    ae.getMessage());
-                                            ae.printStackTrace();
+                                            try {
+                                                String secret =
+                                                        client.getTokens().getDeviceSecret();
+                                                String subject =
+                                                        client.getTokens().getIdToken();
+                                                Log.d(TAG, "secret: " + secret);
+                                                Log.d(TAG, "subject: " + subject);
+                                                saveNativeSSOTokens(new NativeSSOTokens(
+                                                        secret, subject
+                                                ));
+                                            } catch (AuthorizationException ae) {
+                                                Log.e(TAG, "Error saving client secret: " +
+                                                        ae.getMessage());
+                                                ae.printStackTrace();
+                                            }
                                         }
-                                    }
 
-                                    @Override
-                                    public void onError(String error,
-                                                        AuthorizationException exception) {
-                                        mTvStatus.setText(error);
-                                    }
-                                });
+                                        @Override
+                                        public void onError(String error,
+                                                            AuthorizationException exception) {
+                                            mTvStatus.setText(error);
+                                        }
+                                    });
                             }
                         }
                 );
@@ -854,5 +848,31 @@ public class SampleActivity extends AppCompatActivity implements SignInDialog.Si
                 || Build.MANUFACTURER.contains("Google")
                 || Build.PRODUCT.contains("sdk_gphone")
                 || Build.DEVICE.contains("generic");
+    }
+
+    private static class NativeSSOTokens {
+        String secret;
+        String subject;
+
+        NativeSSOTokens(String secret, String subject) {
+            this.secret = secret;
+            this.subject = subject;
+        }
+    }
+
+    private void saveNativeSSOTokens(@NotNull NativeSSOTokens tokens) {
+        getSharedPreferences(SampleActivity.class.getName(), MODE_PRIVATE)
+                .edit().putString(PREF_DEV_SECRET_ACTOR, tokens.secret).apply();
+        getSharedPreferences(SampleActivity.class.getName(), MODE_PRIVATE)
+                .edit().putString(PREF_DEV_SECRET_SUBJECT, tokens.subject).apply();
+    }
+
+    @NotNull
+    private NativeSSOTokens getNativeSSOTokens() {
+        String actor = getSharedPreferences(SampleActivity.class.getName(), MODE_PRIVATE)
+                .getString(PREF_DEV_SECRET_ACTOR, null);
+        String subject = getSharedPreferences(SampleActivity.class.getName(), MODE_PRIVATE)
+                .getString(PREF_DEV_SECRET_SUBJECT, null);
+        return new NativeSSOTokens(actor, subject);
     }
 }
